2023ff7ee97fdf198f98aeef6bebef1cea096577
Arfah Aadil <111839694+Arfah-Aadil@users.noreply.github.com>
2025-03-24 17:37:23 +0000

    Add files via upload

---
+++ AssessmentTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assessment.Tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
